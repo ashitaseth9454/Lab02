2fe501bec711c393b53f5a77a1b3d873202e2f45
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2019-08-19 14:51:58 +0530

    add object:(Scanner, integer)

--- src/ques3/Question3.java
+++ src/ques3/Question3.java
@@ -1,7 +1,12 @@
 package ques3;
 
+import java.util.Scanner;
+
 public class Question3 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        scanner.close();
     }
 }
