fece8645c03da07714d9175c62805a7f2775c143
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2019-08-19 14:43:16 +0530

    input of two strings

--- src/ques2/Question2.java
+++ src/ques2/Question2.java
@@ -1,7 +1,12 @@
 package ques2;
 
+import java.util.Scanner;
+
 public class Question2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String value1 = scanner.nextLine().trim();
+        String value2 = scanner.nextLine().trim();
 
     }
 }
