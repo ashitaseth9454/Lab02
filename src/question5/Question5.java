3038705be7d6db616e147220930e9e0648f4ba4f
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2019-08-19 15:16:09 +0530

    add condition statement and output statements

--- src/question5/Question5.java
+++ src/question5/Question5.java
@@ -1,7 +1,15 @@
 package question5;
 
+import java.util.Scanner;
+
 public class Question5 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        if (num % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
     }
 }
