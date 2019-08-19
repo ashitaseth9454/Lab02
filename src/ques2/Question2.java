0b4ebafc21a2a3511ca784a43146f68cc6ccfc29
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2019-08-19 14:44:51 +0530

    add output statement

--- src/ques2/Question2.java
+++ src/ques2/Question2.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 public class Question2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String value1 = scanner.nextLine().trim();
-        String value2 = scanner.nextLine().trim();
+        String value1 = scanner.next().trim();
+        String value2 = scanner.next().trim();
+        System.out.println(value1 + "University" + value2);
+        scanner.close();
 
     }
 }
