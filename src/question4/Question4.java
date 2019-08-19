69a5503176d73a2d09e2cc8cb4df785e3d442b0d
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2019-08-19 15:10:31 +0530

    add output statement

--- src/question4/Question4.java
+++ src/question4/Question4.java
@@ -1,7 +1,17 @@
 package question4;
 
+import java.util.Scanner;
+
 public class Question4 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        if (num == 0) {
+            System.out.println("Zero");
+        } else if (num > 0) {
+            System.out.println("Positive");
+        } else {
+            System.out.println("Negative");
+        }
     }
 }
