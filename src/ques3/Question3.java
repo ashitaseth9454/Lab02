27aa5b1933ef62fda85dad57333a8f12850d1826
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2019-08-19 15:01:27 +0530

    add output statement

--- src/ques3/Question3.java
+++ src/ques3/Question3.java
@@ -7,6 +7,12 @@ public class Question3 {
         Scanner scanner = new Scanner(System.in);
         int num1 = scanner.nextInt();
         int num2 = scanner.nextInt();
+        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+        System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+        System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+        System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+        System.out.println(num1 + " % " + num2 + " = " + (num1 % num2));
+
         scanner.close();
     }
 }
