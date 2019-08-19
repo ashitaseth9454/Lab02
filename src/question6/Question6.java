ee7e3641a447f619fc992879c041b806abe95bca
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2019-08-19 15:31:02 +0530

    add output statements

--- src/question6/Question6.java
+++ src/question6/Question6.java
@@ -1,7 +1,17 @@
 package question6;
 
+import java.util.Scanner;
+
 public class Question6 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        char value1 = scanner.next().trim().charAt(0);
+        char value2 = scanner.next().trim().charAt(0);
+        if (value1 - value2 > 0) {
+            System.out.println(value2 + " " + value1);
+        } else {
+            System.out.println(value1 + " " + value2);
+        }
+        scanner.close();
     }
 }
